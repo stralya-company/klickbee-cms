e0b83d90a95cbe3f71d5c6285ac630ff73327266
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-06-16 18:27:42 +0200

    docs: updated readme with new architecture of admin

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,21 +6,22 @@ const AUTH_COOKIE_NAME = "session";
 export function middleware(req: NextRequest) {
 	const { pathname } = req.nextUrl;
 
-	const match = pathname.match(/^\/admin_([^/]+)/);
+	const match = pathname.match(/^\/admin\/([^/]+)/);
 	if (!match) return NextResponse.next();
 
 	const key = match[1];
+
 	if (key !== ADMIN_KEY) {
 		return NextResponse.redirect(new URL("/", req.url));
 	}
 
-	const isLoginPage = pathname.startsWith(`/admin_${ADMIN_KEY}/auth/login`);
+	const isLoginPage = pathname.startsWith(`/admin/${ADMIN_KEY}/auth/login`);
 	const isAuthenticated = Boolean(req.cookies.get(AUTH_COOKIE_NAME));
 
 	// 🚫 Redirect only if NOT on login page AND not authenticated
 	if (!isAuthenticated && !isLoginPage) {
 		return NextResponse.redirect(
-			new URL(`/admin_${ADMIN_KEY}/auth/login`, req.url),
+			new URL(`/admin/${ADMIN_KEY}/auth/login`, req.url),
 		);
 	}
 
@@ -28,5 +29,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-	matcher: ["/((?!api|_next|static|favicon.ico).*)"],
+	matcher: ["/admin/:adminKey/:path*"],
 };
