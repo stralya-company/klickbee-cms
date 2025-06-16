994cbbe66ff60178a0a5d08bcfada5a005e9aee2
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-06-16 10:33:13 +0200

    feat: added npm semantic release to automatically update version of the app

--- release.config.js
+++ release.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-    branches: ['main'],
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
-        '@semantic-release/github'
-    ],
-    preset: 'conventionalcommits'
-}
+	branches: ["main"],
+	"plugins": ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/changelog", "@semantic-release/npm", "@semantic-release/github"],
+	preset: "conventionalcommits"
+};
