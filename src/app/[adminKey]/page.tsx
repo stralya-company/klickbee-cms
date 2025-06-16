68dd901ea487f9475c6e26f18f3fb8cefa3f5e55
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-06-16 17:52:58 +0200

    fix: remove useless params and replace by store for admin key

--- src/app/[adminKey]/page.tsx
+++ src/app/[adminKey]/page.tsx
@@ -1,13 +1,20 @@
 "use client";
-import { useState } from "react";
+import React from "react";
+import { useCurrentUser } from "@/lib/queries/useUser";
+import { useAdminKeyStore } from "@/lib/stores/storeAdminKey";
 
-export default function AdminPage({
-	params,
-}: {
-	params: { adminKey: string };
-}) {
-	const [adminKey] = useState(params.adminKey);
+export default function AdminPage() {
+	const adminKey = useAdminKeyStore((state) => state.adminKey);
+	const { data: currentUser } = useCurrentUser();
 	if (!adminKey) {
 		/** Just for dev purposes, in production this should never happen */
 	}
+	return (
+		<div className="flex flex-col min-h-screen items-center justify-center bg-muted">
+			<h1 className="text-2xl font-bold">
+				Admin Page with key {adminKey}
+			</h1>
+			{currentUser && <p>Current user id is : {currentUser.id}</p>}
+		</div>
+	);
 }
