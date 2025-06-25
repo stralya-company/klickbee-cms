1e7f265ecfc9e4e8097fc63c42ae884cdfb145c7
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-06-25 11:49:55 +0200

    fix: remove alias for prisma client to avoid path issues

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "./../../src/generated/prisma/client";
 
 export const prisma = new PrismaClient({
 	// log: ["query", "info", "warn", "error"],
