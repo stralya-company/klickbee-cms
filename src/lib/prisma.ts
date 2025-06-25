fc3e6bc8998f62516bbc061c9c68290158fe0248
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-06-25 11:56:57 +0200

    fix: format

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,15 +3,15 @@ import { PrismaClient } from "./../../src/generated/prisma/client";
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient({
-        // log: ["query", "info", "warn", "error"],
-    });
+	prisma = new PrismaClient({
+		// log: ["query", "info", "warn", "error"],
+	});
 } else {
-    if (!globalThis.prisma) {
-        globalThis.prisma = new PrismaClient({
-            // log: ["query", "info", "warn", "error"],
-        });
-    }
-    prisma = globalThis.prisma;
+	if (!globalThis.prisma) {
+		globalThis.prisma = new PrismaClient({
+			// log: ["query", "info", "warn", "error"],
+		});
+	}
+	prisma = globalThis.prisma;
 }
 export default prisma;
