a057853489b1040850d61e869c549d87606efa4f
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-06-25 11:30:51 +0200

    fix: simplified prisma lib

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,7 @@
 import { PrismaClient } from "@/generated/prisma/client";
-// import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-export const prisma =
-	globalForPrisma.prisma ||
-	new PrismaClient({
-		// log: ["query", "info", "warn", "error"],
-	});
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient({
+	// log: ["query", "info", "warn", "error"],
+});
 
 export default prisma;
