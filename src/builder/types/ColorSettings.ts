be68501c68a17bc4bda3397311f2c24d618859d2
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-07-16 10:05:41 +0200

    refactor: color type, added primary, secondary, accent or unique key

--- src/builder/types/ColorSettings.ts
+++ src/builder/types/ColorSettings.ts
@@ -1,11 +1,16 @@
 import { z } from "zod";
 
+export const colorTypes = ["primary", "secondary", "accent", "unique"] as const;
+export type ColorType = (typeof colorTypes)[number];
+
 export type ColorSettings = {
-	hexCode: string; // e.g. "#ff5733"
-	name: string; // e.g. "Primary Red"
+	hexCode: string;
+	name: string;
+	type: ColorType;
 };
 
 export const colorSchema = z.object({
 	hexCode: z.string().regex(/^#([0-9a-fA-F]{3}){1,2}$/, "Hex invalide"),
 	name: z.string(),
+	type: z.enum(colorTypes),
 });
