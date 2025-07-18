6b5402c5dbc6c7113702f195f5087e8aaac40533
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-07-18 16:37:54 +0200

    feat: implement internationalization for login and settings pages

--- src/app/admin/[adminKey]/layout.tsx
+++ src/app/admin/[adminKey]/layout.tsx
@@ -1,5 +1,10 @@
 "use client";
-import { NextIntlClientProvider, useMessages, useLocale } from "next-intl";
+import {
+	NextIntlClientProvider,
+	useMessages,
+	useLocale,
+	useTranslations,
+} from "next-intl";
 import { UserProvider } from "@/providers/UserProvider";
 import { Button } from "@/components/ui/button";
 import { useUserStore } from "@/feature/user/stores/storeUser";
@@ -14,6 +19,7 @@ export default function AdminLayout({
 	const currentUser = useUserStore((state) => state.user);
 	const messages = useMessages();
 	const locale = useLocale();
+	const t = useTranslations("AdminLayout");
 
 	return (
 		<UserProvider>
@@ -27,7 +33,7 @@ export default function AdminLayout({
 					<main className="flex-1">
 						{currentUser && (
 							<Button variant="ghost" className="mb-4" asChild>
-								<a href="/api/auth/logout">Logout</a>
+								<a href="/api/auth/logout">{t("Logout")}</a>
 							</Button>
 						)}
 						{children}
