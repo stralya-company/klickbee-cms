9b104332191a63ca5fb5530ccd50d6dd5659d853
Jean-Philippe <jp.ouvrard@lesoctetslibres.com>
2025-07-21 15:10:29 +0200

    feat: add email settings page

--- src/app/admin/[adminKey]/manage/settings/email/page.tsx
+++ src/app/admin/[adminKey]/manage/settings/email/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import { Label } from "@/components/ui/label";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useSendEmail } from "@/feature/send-email/queries/useSendEmail";
 import {
 	useSetSetting,
 	useSetting,
 } from "@/feature/settings/queries/useSettings";
-import { Button } from "@/components/ui/button";
-import { useForm } from "react-hook-form";
-import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useState } from "react";
 
 const emailSettingsSchema = z.object({
 	emailHost: z.string(),
+	emailPort: z.string(),
 	emailSender: z.string(),
 	emailUsername: z.string(),
 	emailPassword: z.string().optional(),
@@ -21,46 +24,68 @@ const emailSettingsSchema = z.object({
 
 type EmailSettingsSchema = z.infer<typeof emailSettingsSchema>;
 
+const checkConnectionSchema = z.object({
+	to: z.string(),
+	subject: z.string(),
+	text: z.string(),
+});
+
+type CheckConnectionSchema = z.infer<typeof checkConnectionSchema>;
+
 export default function EmailSettingsPage() {
 	const emailHost = useSetting("emailHost");
+	const emailPort = useSetting("emailPort");
 	const emailSender = useSetting("emailSender");
 	const emailUsername = useSetting("emailUsername");
 	const setSetting = useSetSetting();
-	const {
-		register,
-		handleSubmit,
-		formState: { errors, isSubmitting },
-		reset,
-	} = useForm<EmailSettingsSchema>({
+	const emailSettingsForm = useForm<EmailSettingsSchema>({
 		resolver: zodResolver(emailSettingsSchema),
+	});
+	const checkConnectionForm = useForm<CheckConnectionSchema>({
+		resolver: zodResolver(checkConnectionSchema),
 	});
 	const [emailSettings, setEmailSettings] =
 		useState<EmailSettingsSchema | null>(null);
+	const sendEmail = useSendEmail();
 
 	useEffect(() => {
-		if (emailHost.data && emailSender.data && emailUsername.data) {
+		if (
+			emailHost.data &&
+			emailPort.data &&
+			emailSender.data &&
+			emailUsername.data
+		) {
 			setEmailSettings({
 				emailHost: emailHost.data.value ?? "",
+				emailPort: emailPort.data.value ?? "",
 				emailSender: emailSender.data.value ?? "",
 				emailUsername: emailUsername.data.value ?? "",
 			});
-			reset({
+			emailSettingsForm.reset({
 				emailHost: emailHost.data.value ?? "",
+				emailPort: emailPort.data.value ?? "",
 				emailSender: emailSender.data.value ?? "",
 				emailUsername: emailUsername.data.value ?? "",
 			});
 		}
-	}, [emailHost.data, emailSender.data, emailUsername.data, reset]);
+	}, [
+		emailHost.data,
+		emailPort.data,
+		emailSender.data,
+		emailUsername.data,
+		emailSettingsForm,
+	]);
 
 	if (
 		emailHost.isLoading ||
+		emailPort.isLoading ||
 		emailSender.isLoading ||
 		emailUsername.isLoading
 	) {
 		return <p className="p-4">Chargement...</p>;
 	}
 
-	const onSubmit = (data: EmailSettingsSchema) => {
+	const onEmailSettingsSubmit = (data: EmailSettingsSchema) => {
 		const keys = Object.keys(data) as (keyof EmailSettingsSchema)[];
 
 		keys.forEach((key) => {
@@ -74,50 +99,151 @@ export default function EmailSettingsPage() {
 		});
 	};
 
+	const onCheckConnectionSubmit = (data: CheckConnectionSchema) => {
+		sendEmail.mutate({
+			to: data.to,
+			subject: data.subject,
+			text: data.text,
+		});
+	};
+
 	return (
 		<div className="w-full">
 			<h1 className="p-4">Email Settings Page</h1>
-			<form onSubmit={handleSubmit(onSubmit)} className="p-4 w-xl">
-				<Label htmlFor="emailHost">Email Host</Label>
-				<Input type="text" id="emailHost" {...register("emailHost")} />
-				{errors.emailHost && (
-					<p className="text-red-500">{errors.emailHost.message}</p>
-				)}
-				<Label htmlFor="emailSender">Email Sender</Label>
-				<Input
-					type="text"
-					id="emailSender"
-					{...register("emailSender")}
-				/>
-				{errors.emailSender && (
-					<p className="text-red-500">{errors.emailSender.message}</p>
-				)}
-				<Label htmlFor="emailUsername">Email Username</Label>
-				<Input
-					type="text"
-					id="emailUsername"
-					{...register("emailUsername")}
-				/>
-				{errors.emailUsername && (
-					<p className="text-red-500">
-						{errors.emailUsername.message}
-					</p>
-				)}
-				<Label htmlFor="emailPassword">Email Password</Label>
-				<Input
-					type="password"
-					id="emailPassword"
-					{...register("emailPassword")}
-				/>
-				{errors.emailPassword && (
-					<p className="text-red-500">
-						{errors.emailPassword.message}
-					</p>
-				)}
-				<Button type="submit" disabled={isSubmitting}>
-					Save
-				</Button>
-			</form>
+			<div className="flex gap-3">
+				<form
+					onSubmit={emailSettingsForm.handleSubmit(
+						onEmailSettingsSubmit,
+					)}
+					className="p-4 w-1/2"
+				>
+					<Label htmlFor="emailHost">Email Host</Label>
+					<Input
+						type="text"
+						id="emailHost"
+						{...emailSettingsForm.register("emailHost")}
+					/>
+					{emailSettingsForm.formState.errors.emailHost && (
+						<p className="text-red-500">
+							{
+								emailSettingsForm.formState.errors.emailHost
+									.message
+							}
+						</p>
+					)}
+					<Label htmlFor="emailPort">Email Port</Label>
+					<Input
+						type="text"
+						id="emailPort"
+						{...emailSettingsForm.register("emailPort")}
+					/>
+					{emailSettingsForm.formState.errors.emailPort && (
+						<p className="text-red-500">
+							{
+								emailSettingsForm.formState.errors.emailPort
+									.message
+							}
+						</p>
+					)}
+					<Label htmlFor="emailSender">Email Sender</Label>
+					<Input
+						type="text"
+						id="emailSender"
+						{...emailSettingsForm.register("emailSender")}
+					/>
+					{emailSettingsForm.formState.errors.emailSender && (
+						<p className="text-red-500">
+							{
+								emailSettingsForm.formState.errors.emailSender
+									.message
+							}
+						</p>
+					)}
+					<Label htmlFor="emailUsername">Email Username</Label>
+					<Input
+						type="text"
+						id="emailUsername"
+						{...emailSettingsForm.register("emailUsername")}
+					/>
+					{emailSettingsForm.formState.errors.emailUsername && (
+						<p className="text-red-500">
+							{
+								emailSettingsForm.formState.errors.emailUsername
+									.message
+							}
+						</p>
+					)}
+					<Label htmlFor="emailPassword">Email Password</Label>
+					<Input
+						type="password"
+						id="emailPassword"
+						{...emailSettingsForm.register("emailPassword")}
+					/>
+					{emailSettingsForm.formState.errors.emailPassword && (
+						<p className="text-red-500">
+							{
+								emailSettingsForm.formState.errors.emailPassword
+									.message
+							}
+						</p>
+					)}
+					<Button
+						type="submit"
+						disabled={emailSettingsForm.formState.isSubmitting}
+					>
+						Save
+					</Button>
+				</form>
+				<form
+					onSubmit={checkConnectionForm.handleSubmit(
+						onCheckConnectionSubmit,
+					)}
+					className="p-4 w-1/2"
+				>
+					<Label htmlFor="to">To</Label>
+					<Input
+						type="text"
+						id="to"
+						{...checkConnectionForm.register("to")}
+					/>
+					{checkConnectionForm.formState.errors.to && (
+						<p className="text-red-500">
+							{checkConnectionForm.formState.errors.to.message}
+						</p>
+					)}
+					<Label htmlFor="subject">Subject</Label>
+					<Input
+						type="text"
+						id="subject"
+						{...checkConnectionForm.register("subject")}
+					/>
+					{checkConnectionForm.formState.errors.subject && (
+						<p className="text-red-500">
+							{
+								checkConnectionForm.formState.errors.subject
+									.message
+							}
+						</p>
+					)}
+					<Label htmlFor="text">Text</Label>
+					<Input
+						type="text"
+						id="text"
+						{...checkConnectionForm.register("text")}
+					/>
+					{checkConnectionForm.formState.errors.text && (
+						<p className="text-red-500">
+							{checkConnectionForm.formState.errors.text.message}
+						</p>
+					)}
+					<Button
+						type="submit"
+						disabled={checkConnectionForm.formState.isSubmitting}
+					>
+						Check connection
+					</Button>
+				</form>
+			</div>
 		</div>
 	);
 }
