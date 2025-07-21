b0682cb907a2cfc3f254f063253a3dc20d26984e
Kaluxxx <allard.lucas.97@gmail.com>
2025-07-21 20:03:37 +0200

    feat: add userPasswordResetRequestSchema for email validation in password reset requests #56

---
+++ src/feature/user/types/userPasswordResetRequestSchema.ts
@@ -0,0 +1,8 @@
+import z from "zod";
+
+export const userPasswordResetRequestSchema = z.object({
+	email: z
+		.string()
+		.email({ message: "Invalid email address" })
+		.min(1, { message: "Email is required" }),
+});
