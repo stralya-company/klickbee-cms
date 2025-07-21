d2f31ff6d3543f68283e40b233a94d9d4b8ad141
Kaluxxx <allard.lucas.97@gmail.com>
2025-07-21 22:53:18 +0200

    feat: export UserPasswordResetRequestSchema type from userPasswordResetRequestSchema #56

--- src/feature/user/types/userPasswordResetRequestSchema.ts
+++ src/feature/user/types/userPasswordResetRequestSchema.ts
@@ -6,3 +6,7 @@ export const userPasswordResetRequestSchema = z.object({
 		.email({ message: "Invalid email address" })
 		.min(1, { message: "Email is required" }),
 });
+
+export type UserPasswordResetRequestSchema = z.infer<
+	typeof userPasswordResetRequestSchema
+>;
