d552ffd21b6dcb530a9ae919345ffe39e30bc5f2
Kaluxxx <allard.lucas.97@gmail.com>
2025-07-21 23:54:44 +0200

    refactor: streamline deletePasswordResetRequest function by removing error handling #56

--- src/feature/user/functions/deletePasswordResetRequest.ts
+++ src/feature/user/functions/deletePasswordResetRequest.ts
@@ -3,12 +3,7 @@ import prisma from "@/lib/prisma";
 export default async function deletePasswordResetRequest(
 	token: string,
 ): Promise<void> {
-	try {
-		await prisma.userPasswordReset.delete({
-			where: { token },
-		});
-	} catch (error) {
-		console.error("Failed to delete password reset request:", error);
-		throw error; // Re-throw the error to propagate it to the caller
-	}
+	await prisma.userPasswordReset.delete({
+		where: { token },
+	});
 }
