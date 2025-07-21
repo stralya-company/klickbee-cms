fe9015ae9f7dc5397308e7fbc82aad308494b995
Jean-Philippe <jp.ouvrard@lesoctetslibres.com>
2025-07-21 17:56:20 +0200

    fix: fix prettier error

--- src/feature/send-email/api/apiSendEmail.ts
+++ src/feature/send-email/api/apiSendEmail.ts
@@ -40,6 +40,9 @@ export async function POST(req: NextRequest) {
 		return NextResponse.json({ message: "Email sent with success" });
 	} catch (err) {
 		console.error(err);
-		return NextResponse.json({ error: "Server error while sending email" }, { status: 500 });
+		return NextResponse.json(
+			{ error: "Server error while sending email" },
+			{ status: 500 },
+		);
 	}
 }
