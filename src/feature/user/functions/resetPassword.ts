e65058ff3efe13fdad199545551c5b5a1f301727
Kaluxxx <allard.lucas.97@gmail.com>
2025-07-21 20:04:54 +0200

    feat: implement resetPassword function for handling password resets #56

---
+++ src/feature/user/functions/resetPassword.ts
@@ -0,0 +1,33 @@
+import prisma from "@/lib/prisma";
+import bcrypt from "bcrypt";
+
+export default async function resetPassword(
+	token: string,
+	newPassword: string,
+): Promise<boolean> {
+	const resetRequest = await prisma.userPasswordReset.findUnique({
+		where: { token },
+	});
+
+	if (!resetRequest) {
+		throw new Error("Password reset request not found");
+	}
+
+	const user = await prisma.user.findUnique({
+		where: { id: resetRequest.userId },
+		select: { id: true, email: true },
+	});
+
+	if (!user) {
+		throw new Error("User not found");
+	}
+
+	const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+	await prisma.user.update({
+		where: { id: user.id },
+		data: { password: hashedPassword },
+	});
+
+	return true;
+}
