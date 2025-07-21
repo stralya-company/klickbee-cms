76d56e43eb801b54872920f9437c36bfad6cdd52
Kaluxxx <allard.lucas.97@gmail.com>
2025-07-21 20:28:20 +0200

    fix: change deletePasswordResetRequest function return type from boolean to void and improve error handling #56

--- src/feature/user/functions/deletePasswordResetRequest.ts
+++ src/feature/user/functions/deletePasswordResetRequest.ts
@@ -2,10 +2,13 @@ import prisma from "@/lib/prisma";
 
 export default async function deletePasswordResetRequest(
 	token: string,
-): Promise<boolean> {
-	await prisma.userPasswordReset.delete({
-		where: { token },
-	});
-
-	return true;
+): Promise<void> {
+	try {
+		await prisma.userPasswordReset.delete({
+			where: { token },
+		});
+	} catch (error) {
+		console.error("Failed to delete password reset request:", error);
+		throw error; // Re-throw the error to propagate it to the caller
+	}
 }
