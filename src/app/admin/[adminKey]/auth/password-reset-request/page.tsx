002f6e090c247b139789acd58bedd44e00de950b
Kaluxxx <allard.lucas.97@gmail.com>
2025-07-21 23:28:07 +0200

    feat: add localization to reset password request page title #56

--- src/app/admin/[adminKey]/auth/password-reset-request/page.tsx
+++ src/app/admin/[adminKey]/auth/password-reset-request/page.tsx
@@ -1,12 +1,15 @@
 import ResetPasswordRequestForm from "@/components/admin/auth/reset-password-request/resetPasswordRequestForm";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useTranslations } from "next-intl";
 
 export default function ResetPasswordRequestPage() {
+	const t = useTranslations("ResetPasswordRequest");
+
 	return (
 		<div className="flex min-h-screen items-center justify-center bg-muted w-full">
 			<Card className="w-full max-w-sm">
 				<CardHeader>
-					<CardTitle>Réinitialisation du mot de passe</CardTitle>
+					<CardTitle>{t("ResetPasswordRequest")}</CardTitle>
 				</CardHeader>
 				<CardContent>
 					<ResetPasswordRequestForm />
