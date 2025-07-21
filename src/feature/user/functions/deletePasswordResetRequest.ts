e62996d7ca177a67f3b5d649581eb27733d3ca93
Kaluxxx <allard.lucas.97@gmail.com>
2025-07-21 20:12:45 +0200

    fix: simplify deletePasswordResetRequest function by removing unnecessary result check #56

--- src/feature/user/functions/deletePasswordResetRequest.ts
+++ src/feature/user/functions/deletePasswordResetRequest.ts
@@ -3,13 +3,9 @@ import prisma from "@/lib/prisma";
 export default async function deletePasswordResetRequest(
 	token: string,
 ): Promise<boolean> {
-	const result = await prisma.userPasswordReset.delete({
+	await prisma.userPasswordReset.delete({
 		where: { token },
 	});
-
-	if (!result) {
-		throw new Error("Failed to delete password reset request");
-	}
 
 	return true;
 }
