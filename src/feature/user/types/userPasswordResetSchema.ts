4b52849acc82e6f9621453155c14a2f731eeca91
Kaluxxx <allard.lucas.97@gmail.com>
2025-07-22 12:27:26 +0200

    feat: add confirmation field and validation for password reset schema + add type for formValues #56

--- src/feature/user/types/userPasswordResetSchema.ts
+++ src/feature/user/types/userPasswordResetSchema.ts
@@ -1,6 +1,15 @@
 import z from "zod";
 
-export const userPasswordResetSchema = z.object({
-	token: z.string().uuid().min(1),
-	newPassword: z.string().min(8),
-});
+export const userPasswordResetSchema = z
+	.object({
+		token: z.string().uuid().min(1),
+		newPassword: z.string().min(8),
+		confirmNewPassword: z.string().min(8),
+	})
+	.refine((data) => data.newPassword === data.confirmNewPassword, {
+		path: ["confirmNewPassword"],
+	});
+
+export type UserPasswordResetFormValues = z.infer<
+	typeof userPasswordResetSchema
+>;
