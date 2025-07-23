cceddb68b619eec23ea2d5669e0ce12d60cbb402
Lucas <allard.lucas.97@gmail.com>
2025-07-23 21:41:10 +0200

    fix: remove uuid validation from password reset token to support non-UUID tokens #78

--- src/feature/user/types/userPasswordResetSchema.ts
+++ src/feature/user/types/userPasswordResetSchema.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const userPasswordResetSchema = z
 	.object({
-		token: z.string().uuid().min(1),
+		token: z.string().min(1),
 		newPassword: z.string().min(8),
 		confirmNewPassword: z.string().min(8),
 	})
