faa7a30da34cb5b071b62dccad8c3afb2186032f
Lucas <allard.lucas.97@gmail.com>
2025-07-23 21:40:54 +0200

    feat: add sendResetPassword method with email support to better-auth configuration #78

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,8 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/prisma";
 import bcrypt from "bcrypt";
 import { admin } from "better-auth/plugins";
+import { sendEmail } from "@/lib/sendEmail";
+import { getApiTranslation } from "@/lib/apiTranslation";
 
 export const auth = betterAuth({
 	database: prismaAdapter(prisma, {
@@ -25,6 +27,27 @@ export const auth = betterAuth({
 			}) => {
 				return await bcrypt.compare(password, hash);
 			},
+		},
+		sendResetPassword: async ({ user, url, token }) => {
+			const emailSubject = await getApiTranslation(
+				"PasswordResetRequest",
+				"EmailSubject",
+			);
+			const emailContent = await getApiTranslation(
+				"PasswordResetRequest",
+				"EmailContent",
+			);
+
+			try {
+				await sendEmail({
+					to: user.email,
+					subject: emailSubject,
+					text: `${emailContent}: ${url}?token=${token}`,
+				});
+			} catch (error) {
+				console.error("Error sending reset password email:", error);
+				throw new Error("Failed to send reset password email.");
+			}
 		},
 	},
 	plugins: [admin()],
