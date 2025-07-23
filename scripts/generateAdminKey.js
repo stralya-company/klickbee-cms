f67f30b17e335ff65d456b6186836dde9974efc0
jpouvrard <jpouvrard.dev@proton.me>
2025-07-23 17:38:13 +0200

    refactor: add console.log back

--- scripts/generateAdminKey.js
+++ scripts/generateAdminKey.js
@@ -15,4 +15,13 @@ if (!env.includes(keyName)) {
 	const newKey = randomUUID();
 	env += `\n${keyName}=admin_${newKey}\n`;
 	fs.writeFileSync(envPath, env);
+	// biome-ignore lint/suspicious/noConsole: usefull feedback for users
+	console.log(
+		`A new admin key has been generated and added to the .env file: admin_${newKey}`,
+	);
+} else {
+	// biome-ignore lint/suspicious/noConsole: usefull feedback for users
+	console.log(
+		`The admin key already exists in the .env file. No changes were made.`,
+	);
 }
