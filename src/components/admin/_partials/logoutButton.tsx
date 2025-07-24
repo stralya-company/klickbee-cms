a40fc863656267f6ebc8e03b55e4302d3b6372bd
kaluxxx <100565368+kaluxxx@users.noreply.github.com>
2025-07-24 19:43:20 +0200

    feat: add logout button component for admin user authentication

---
+++ src/components/admin/_partials/logoutButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useTranslations } from "next-intl";
+import { Button } from "@/components/ui/button";
+import { useAdminKeyStore } from "@/feature/admin-key/stores/storeAdminKey";
+import { authClient } from "@/lib/authClient";
+
+export default function LogoutButton() {
+	const router = useRouter();
+	const adminKey = useAdminKeyStore((state) => state.adminKey);
+	const t = useTranslations("AdminLayout");
+
+	async function handleLogout() {
+		await authClient.signOut({
+			fetchOptions: {
+				onSuccess: () => {
+					router.push(`admin/${adminKey}/auth/login`);
+				},
+			},
+		});
+	}
+
+	return (
+		<Button className="mb-4" onClick={handleLogout} variant="ghost">
+			{t("Logout")}
+		</Button>
+	);
+}
