c5a4ea5ea37df23999e4c76958d6c0cff1cb803d
kaluxxx <100565368+kaluxxx@users.noreply.github.com>
2025-07-25 00:22:11 +0200

    feat: implement user management page with search, action button, and pagination

---
+++ src/app/admin/[adminKey]/(dashboard)/manage/settings/users/page.tsx
@@ -0,0 +1,35 @@
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { useTranslations } from "next-intl";
+import { DashboardTitle } from "@/components/admin/_partials/dashboardTitle";
+import ActionButton from "@/components/admin/settings/users/actionButton";
+import Pagination from "@/components/admin/settings/users/pagination";
+import SearchBar from "@/components/admin/settings/users/searchBar";
+import UsersTable from "@/components/admin/settings/users/usersTable";
+import { getQueryClient } from "@/lib/react-query/getQueryClient";
+import { allUsersOptions } from "@/lib/react-query/query-options/users/allUsersOptions";
+
+export default function Page() {
+	const t = useTranslations("SettingsUsers");
+
+	const queryClient = getQueryClient();
+	void queryClient.prefetchQuery(allUsersOptions);
+
+	return (
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<section className="flex flex-col gap-4">
+				<DashboardTitle
+					subtitle={t("ManageUsersSubtitle")}
+					title={t("ManageUsers")}
+				/>
+				<div className="p-12 space-y-4">
+					<div className="flex items-center justify-between gap-4">
+						<SearchBar />
+						<ActionButton />
+					</div>
+					<UsersTable />
+					<Pagination />
+				</div>
+			</section>
+		</HydrationBoundary>
+	);
+}
