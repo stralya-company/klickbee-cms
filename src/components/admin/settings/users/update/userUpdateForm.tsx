a404ba6ae8d961c2c86c15101edab772dc93c238
kaluxxx <100565368+kaluxxx@users.noreply.github.com>
2025-07-25 14:38:09 +0200

    feat: add UserCreateForm and UserUpdateForm components for user management

---
+++ src/components/admin/settings/users/update/userUpdateForm.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useTranslations } from "next-intl";
+import { type FieldValues } from "react-hook-form";
+import { toast } from "sonner";
+import UserForm from "@/components/admin/settings/users/userForm";
+import { useAdminKeyStore } from "@/feature/admin-key/stores/storeAdminKey";
+import { useUpdateUser } from "@/feature/user/queries/useUpdateUser";
+import { useUser } from "@/feature/user/queries/useUser";
+import {
+	UpdateUserFormValues,
+	updateUserSchema,
+} from "@/feature/user/types/updateUserSchema";
+import { UserToUpdate } from "@/feature/user/types/user";
+
+interface UserUpdateFormProps {
+	userId: string;
+}
+
+export default function UserUpdateForm({ userId }: UserUpdateFormProps) {
+	const { data: user } = useUser(userId);
+	const t = useTranslations("SettingsUsers");
+	const router = useRouter();
+	const adminKey = useAdminKeyStore((state) => state.adminKey);
+	const updateUserMutation = useUpdateUser();
+
+	const handleSubmit = async (values: FieldValues) => {
+		try {
+			await updateUserMutation.mutateAsync({
+				userId,
+				values: values as UpdateUserFormValues,
+			});
+			toast.success(t("UpdateUserSuccess"));
+			router.push(`/admin/${adminKey}/manage/settings/users`);
+		} catch (error) {
+			console.error("Error updating user:", error);
+			toast.error(t("UpdateUserError"));
+		}
+	};
+
+	if (!user || "error" in user) {
+		return <div>Error loading user</div>;
+	}
+
+	return (
+		<UserForm
+			initialValues={user as UserToUpdate}
+			isSubmitting={updateUserMutation.isPending}
+			onSubmit={handleSubmit}
+			schema={updateUserSchema}
+			showEmailField={false}
+			submitButtonText={t("UpdateUser")}
+			submittingText={t("UpdatingUser")}
+			title={t("UpdateUser")}
+		/>
+	);
+}
