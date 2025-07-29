6a501dca1b705130e1aadd386b509015bb97c55f
Lucas <allard.lucas.97@gmail.com>
2025-07-29 13:36:49 +0200

    feat: add Zustand stores for managing contact search and selection

---
+++ src/feature/contact/stores/storeContactSelection.ts
@@ -0,0 +1,42 @@
+import { create } from "zustand";
+
+interface ContactSelectionStore {
+	selectedItems: string[];
+	addSelection: (id: string) => void;
+	removeSelection: (id: string) => void;
+	toggleSelection: (id: string) => void;
+	clearSelection: () => void;
+	setSelectedItems: (ids: string[]) => void;
+}
+
+export const useContactSelectionStore = create<ContactSelectionStore>(
+	(set, get) => ({
+		addSelection: (id) =>
+			set((state) => ({
+				selectedItems: [...state.selectedItems, id],
+			})),
+		clearSelection: () => set({ selectedItems: [] }),
+		removeSelection: (id) =>
+			set((state) => ({
+				selectedItems: state.selectedItems.filter(
+					(item) => item !== id,
+				),
+			})),
+		selectedItems: [],
+		setSelectedItems: (ids) => set({ selectedItems: ids }),
+		toggleSelection: (id) => {
+			const { selectedItems } = get();
+			if (selectedItems.includes(id)) {
+				set((state) => ({
+					selectedItems: state.selectedItems.filter(
+						(item) => item !== id,
+					),
+				}));
+			} else {
+				set((state) => ({
+					selectedItems: [...state.selectedItems, id],
+				}));
+			}
+		},
+	}),
+);
