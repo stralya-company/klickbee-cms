1920d90637f47bb17f28e7eae4a6e934a1bf2120
Lucas <allard.lucas.97@gmail.com>
2025-07-29 11:53:04 +0200

    feat: refactor UserSearchBar component to utilize shared SearchBar component

--- src/components/admin/settings/users/searchBar.tsx
+++ src/components/admin/settings/users/searchBar.tsx
@@ -1,41 +1,16 @@
 "use client";
 
-import { SearchIcon } from "lucide-react";
-import { useRouter, useSearchParams } from "next/navigation";
 import { useTranslations } from "next-intl";
-import { useDeferredValue, useEffect, useState } from "react";
-import { Input } from "@/components/ui/input";
+import SearchBar from "@/components/admin/_partials/searchBar";
+import { useUserSearchStore } from "@/feature/user/stores/storeUserSearch";
 
-export default function SearchBar() {
+export default function UserSearchBar() {
 	const t = useTranslations("SettingsUsers");
-	const router = useRouter();
-	const searchParams = useSearchParams();
-	const [searchValue, setSearchValue] = useState(
-		searchParams.get("search") || "",
-	);
-	const deferredSearchValue = useDeferredValue(searchValue);
-
-	useEffect(() => {
-		const params = new URLSearchParams(searchParams);
-		if (deferredSearchValue) {
-			params.set("search", deferredSearchValue);
-		} else {
-			params.delete("search");
-		}
-		router.replace(`?${params.toString()}`);
-	}, [deferredSearchValue, router, searchParams]);
 
 	return (
-		<div className="w-full max-w-md flex items-center space-x-2">
-			<div className="relative flex-1">
-				<SearchIcon className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-				<Input
-					className="pl-8"
-					onChange={(e) => setSearchValue(e.target.value)}
-					placeholder={t("SearchUsers")}
-					value={searchValue}
-				/>
-			</div>
-		</div>
+		<SearchBar
+			placeholder={t("SearchUsers")}
+			useSearchStore={useUserSearchStore}
+		/>
 	);
 }
