7b5d1b5792ce50ad25b8d052773f255cf32a839d
Lucas <allard.lucas.97@gmail.com>
2025-07-29 18:31:20 +0200

    fix: remove mock data handling from allContactsOptions and contactByIdOptions

--- src/feature/contact/options/allContactsOptions.ts
+++ src/feature/contact/options/allContactsOptions.ts
@@ -1,13 +1,7 @@
 import { queryOptions } from "@tanstack/react-query";
 import { getAllContacts } from "@/feature/contact/lib/contacts";
-import { mockContacts } from "@/feature/contact/lib/mockContacts";
 
 export const allContactsOptions = queryOptions({
-	queryFn: async () => {
-		if (process.env.NODE_ENV === "development") {
-			return mockContacts;
-		}
-		return await getAllContacts();
-	},
+	queryFn: async () => await getAllContacts(),
 	queryKey: ["all_contacts"] as const,
 });
--- src/feature/contact/options/contactByIdOptions.ts
+++ src/feature/contact/options/contactByIdOptions.ts
@@ -1,14 +1,8 @@
 import { queryOptions } from "@tanstack/react-query";
 import { getContactById } from "@/feature/contact/lib/contacts";
-import { getMockContactById } from "@/feature/contact/lib/mockContacts";
 
 export const contactByIdOptions = (contactId: string) =>
 	queryOptions({
-		queryFn: async () => {
-			if (process.env.NODE_ENV === "development") {
-				return getMockContactById(parseInt(contactId));
-			}
-			return await getContactById(contactId);
-		},
+		queryFn: async () => await getContactById(contactId),
 		queryKey: ["contact", contactId] as const,
 	});
