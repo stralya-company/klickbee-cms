bffb4784b119c23b651078444845e3ceb94b2804
Julien MAUCLAIR <julien.mauclair@e-polaz.com>
2025-08-01 16:35:59 +0200

    feat: fetch homepage data and pass it to BuilderComponent

--- src/app/admin/[adminKey]/builder/page.tsx
+++ src/app/admin/[adminKey]/builder/page.tsx
@@ -1,5 +1,20 @@
 import BuilderComponent from "@/components/builder/Builder";
+import { getPageById } from "@/feature/page/lib/pages";
+import { PageLight } from "@/feature/page/types/page";
+import { getSetting } from "@/feature/settings/lib/settings";
 
-export default function BuilderPage() {
-	return <BuilderComponent />;
+export default async function BuilderPage() {
+	// Fetch homepage ID from settings
+	const homePageIdSetting = await getSetting("current_homepage_id");
+	const homePageId = homePageIdSetting
+		? Number(homePageIdSetting)
+		: undefined;
+
+	// Fetch page data if we have a homepage ID
+	let pageData: PageLight | undefined;
+	if (homePageId) {
+		pageData = (await getPageById(homePageId)) as PageLight;
+	}
+
+	return <BuilderComponent homePageId={homePageId} pageData={pageData} />;
 }
